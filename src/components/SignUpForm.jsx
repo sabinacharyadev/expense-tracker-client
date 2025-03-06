addd9f4a733292877f5fa531d98b719c7c343558
sabinbabu <sabinacharya444@gmail.com>
2025-03-06 13:38:34 +1100

    controlled react form inplemented

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const SignUpForm = () => {
+  const initialData = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [formData, setFormData] = useState(initialData);
+  const { name, email, password, confirmPassword } = formData;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <Form>
       <h2>Sign Up</h2>
 
       <Form.Group className="mb-3" controlId="formBasicText">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
+        <Form.Control
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleOnChange}
+          placeholder="Enter name"
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleOnChange}
+          placeholder="Enter email"
+        />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -20,12 +46,24 @@ const SignUpForm = () => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleOnChange}
+          placeholder="Password"
+        />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
         <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleOnChange}
+          placeholder="Password"
+        />
       </Form.Group>
 
       <Button variant="primary" type="submit">
